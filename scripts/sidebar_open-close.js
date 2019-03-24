79093fee3c5cbd52e6861e5b437ee7ed6daceca5
kttalley <talleykristian@gmail.com>
2019-03-23 16:38:08 -0800

    added sidebar script

---
+++ scripts/sidebar_open-close.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "30%";
+
+    document.getElementById("nav-menu").style.width = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("nav-menu").style.width = "100px";
+}
